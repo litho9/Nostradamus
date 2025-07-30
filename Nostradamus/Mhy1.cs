709cfebbfee486ace6b9e82d81add2aa4d21ecc8
lioh <lithosmances@gmail.com>
2025-07-30 14:06:41 -0300

    LoadCabMap

--- Nostradamus/Mhy1.cs
+++ Nostradamus/Mhy1.cs
@@ -1,31 +1,22 @@
 ﻿using System.Buffers;
+using System.Collections.Concurrent;
 using System.Text;
 using static System.Linq.Enumerable;
 using static Nostradamus.Descrambelhador;
 
 namespace Nostradamus;
 
-public class Blk {
-    public readonly Dictionary<string, Cab> Cabs = new();
+public class Mhy1 {
+    public readonly Dictionary<string, Dictionary<string, Cab>> Blocks = new(); // blkName↝cabName↝Cab
+    private readonly ConcurrentDictionary<string, string> _cabMap = new(); // cabName↝blkName
 
-    public Blk(string fileName) {
-        var stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+    public Dictionary<string, Cab> LoadBlock(string blockName) {
+        var path0 = Environment.GetEnvironmentVariable("GAME_PATH");
+        Dictionary<string, Cab> cabs = new();
+        var stream = File.Open(path0 + blockName, FileMode.Open, FileAccess.Read);
         var reader = new BinaryReader(stream);
         while (stream.Position < stream.Length) {
-            if (!reader.ReadBytes(4).SequenceEqual("mhy1"u8.ToArray()))
-                throw new Exception("File does not begin with 'mhy1'.");
-            var compressed = reader.ReadBytes((int)reader.ReadUInt32());
-            Descramble(compressed, Math.Min(compressed.Length, 128), 28);
-            var size = ReadUInt(compressed[48..(48+7)]); // offset=48 signature=7 
-            var block = ArrayPool<byte>.Shared.Rent(size);
-            OodleLZ(compressed.AsSpan(48+7), block.AsSpan(0, size));
-
-            using var r = new BinaryReader(new MemoryStream(block, 0, size));
-            var nodes = Range(0, ReadInt(r)).Select(_ =>
-                new DirNode(ReadString(r), r.ReadBoolean(), ReadInt(r), ReadUInt(r))).ToList();
-            var blocks = Range(0, ReadInt(r)).Select(_ =>
-                new StorageBlock(ReadInt(r), ReadUInt(r))).ToList();
-            ArrayPool<byte>.Shared.Return(block);
+            var (nodes, blocks) = ReadMhy1Headers(reader);
             if (nodes.Count == 0) {
                 Console.WriteLine($"[MHY1] skipping block {stream.Position:x8}.");
                 stream.Position += blocks.Sum(x => x.CompressedSize);
@@ -34,29 +25,79 @@ public class Blk {
 
             var blocksStream = new MemoryStream(blocks.Sum(x => x.UncompressedSize));
             foreach (var (compressedSize, uncompressedSize) in blocks) {
-                var compressedBytes = ArrayPool<byte>.Shared.Rent(compressedSize);
-                var uncompressedBytes = ArrayPool<byte>.Shared.Rent(uncompressedSize);
-                if (reader.Read(compressedBytes, 0, compressedSize) == 0) throw new Exception("Readn't");
-                Descramble(compressedBytes, Math.Min(compressedSize, 128), 8);
-                OodleLZ(compressedBytes.AsSpan(28, compressedSize - 28), uncompressedBytes); // offset=28
-                blocksStream.Write(uncompressedBytes);
-                ArrayPool<byte>.Shared.Return(compressedBytes);
-                ArrayPool<byte>.Shared.Return(uncompressedBytes);
+                var compressed = ArrayPool<byte>.Shared.Rent(compressedSize);
+                var uncompressed = ArrayPool<byte>.Shared.Rent(uncompressedSize);
+                if (reader.Read(compressed, 0, compressedSize) == 0) throw new Exception("Readn't");
+                Descramble(compressed, Math.Min(compressedSize, 128), 8);
+                OodleLZ(compressed.AsSpan(28, compressedSize - 28), uncompressed); // offset=28
+                blocksStream.Write(uncompressed);
+                ArrayPool<byte>.Shared.Return(compressed);
+                ArrayPool<byte>.Shared.Return(uncompressed);
             }
 
             foreach (var node in nodes) {
-                var fileStr = $"{fileName.Split("\\").Last()}[{stream.Position:x8}]";
+                var fileStr = $"{blockName.Split("\\").Last()}[{stream.Position:x8}]";
                 if (node.Path.EndsWith("resS")) {
                     Console.WriteLine($"[NHY1] Skipping {node.Path} in {fileStr}.");
                     continue;
                 }
                 Console.WriteLine($"[MHY1] Processing {node.Path} in {fileStr}.");
                 blocksStream.Position = node.Offset;
-                Cabs.Add(node.Path, new Cab(blocksStream));
+                cabs.Add(node.Path, new Cab(blocksStream));
             }
         }
+        Blocks.Add(blockName, cabs);
+        return cabs;
+    }
+
+    public void LoadCabMap() {
+        var dir = Environment.GetEnvironmentVariable("GAME_PATH")!;
+        var d = new DirectoryInfo(dir);
+        var files = d.GetFiles("*.blk");
+        Parallel.ForEach(files, file => {
+            try {
+                var stream = File.Open(file.FullName, FileMode.Open, FileAccess.Read);
+                var reader = new BinaryReader(stream);
+                while (stream.Position < stream.Length) {
+                    var (nodes, blocks) = ReadMhy1Headers(reader);
+                    foreach (var n in nodes) _cabMap.TryAdd(n.Path, file.FullName);
+                    stream.Position += blocks.Sum(x => x.CompressedSize);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine($"Error loading {file.Name}");
+            }
+        });
     }
     
+    T Point<T>(PPtr<T> pPtr, Cab cab) {
+        var cab2 = pPtr.FileId == 0 ? cab : GetCab(cab.Externals[pPtr.FileId - 1]);
+        return (T)cab2.Objects[pPtr.PathId];
+    }
+
+    private Cab GetCab(string cabName) {
+        if (!_cabMap.ContainsKey(cabName)) LoadCabMap();
+        if (!Blocks.ContainsKey(_cabMap[cabName])) LoadBlock(_cabMap[cabName]);
+        return Blocks[_cabMap[cabName]][cabName];
+    }
+
+    private (List<DirNode>, List<StorageBlock>) ReadMhy1Headers(BinaryReader reader) {
+        if (!reader.ReadBytes(4).SequenceEqual("mhy1"u8.ToArray()))
+            throw new Exception("File does not begin with 'mhy1'.");
+        var compressed = reader.ReadBytes((int)reader.ReadUInt32());
+        Descramble(compressed, Math.Min(compressed.Length, 128), 28);
+        var size = ReadUInt(compressed[48..(48+7)]); // offset=48 signature=7 
+        var block = ArrayPool<byte>.Shared.Rent(size);
+        OodleLZ(compressed.AsSpan(48+7), block.AsSpan(0, size));
+
+        using var r = new BinaryReader(new MemoryStream(block, 0, size));
+        var nodes = Range(0, ReadInt(r)).Select(_ =>
+            new DirNode(ReadString(r), r.ReadBoolean(), ReadInt(r), ReadUInt(r))).ToList();
+        var blocks = Range(0, ReadInt(r)).Select(_ =>
+            new StorageBlock(ReadInt(r), ReadUInt(r))).ToList();
+        ArrayPool<byte>.Shared.Return(block);
+        return (nodes, blocks);
+    }
+
     private static string ReadStringToNull(BinaryReader reader, int maxLength = 32767) {
         var bytes = new List<byte>();
         var count = 0;
@@ -70,9 +111,11 @@ public class Blk {
         return Encoding.UTF8.GetString(bytes.ToArray());
     }
     private static int ReadInt(BinaryReader r) => ReadInt(r.ReadBytes(6));
-    private static int ReadInt(byte[] buffer) => buffer[2] | (buffer[4] << 8) | (buffer[0] << 0x10) | (buffer[5] << 0x18);
+    private static int ReadInt(byte[] buffer) =>
+        buffer[2] | (buffer[4] << 8) | (buffer[0] << 0x10) | (buffer[5] << 0x18);
     private static int ReadUInt(BinaryReader r) => ReadUInt(r.ReadBytes(7));
-    private static int ReadUInt(byte[] buffer) => buffer[1] | (buffer[6] << 8) | (buffer[3] << 0x10) | (buffer[2] << 0x18);
+    private static int ReadUInt(byte[] buffer) =>
+        buffer[1] | (buffer[6] << 8) | (buffer[3] << 0x10) | (buffer[2] << 0x18);
     private static string ReadString(BinaryReader r) {
         var pos = r.BaseStream.Position;
         var str = ReadStringToNull(r);
--- Nostradamus/Program.cs
+++ Nostradamus/Program.cs
@@ -1,18 +1,24 @@
 ﻿using Nostradamus;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
-var watch = System.Diagnostics.Stopwatch.StartNew();
-Console.WriteLine("** Nostradamus **");
 
-var path0 = Environment.GetEnvironmentVariable("GAME_PATH");
-var blk = new Blk(path0 + "3212318446.blk");
+Console.WriteLine("** Nostradamus **");
+var watch = System.Diagnostics.Stopwatch.StartNew();
+var mhy1 = new Mhy1();
+// mhy1.LoadCabMap();
+Console.WriteLine($"cabMap loaded in {watch.ElapsedMilliseconds}ms.");
+
+watch.Restart();
+var blk = mhy1.LoadBlock("3212318446.blk");
+var cab0 = blk["CAB-0aa2768ea164a0d7db932b50052974af"];
 Console.WriteLine($"blk processed in {watch.ElapsedMilliseconds}ms.");
 
-var blk2 = new Blk(path0 + "2299538835.blk"); // SeparateMesh_Avatar_Female_Size01_Jufufu_Model_Jufufu_Body_1
-var cab2 = blk2.Cabs["CAB-cd0a7c1addc386d573a202aad65e4aff"];
+watch.Restart();
+var blk2 = mhy1.LoadBlock("2299538835.blk"); // SeparateMesh_Avatar_Female_Size01_Jufufu_Model_Jufufu_Body_1
+var cab2 = blk2["CAB-cd0a7c1addc386d573a202aad65e4aff"];
+Console.WriteLine($"blk processed in {watch.ElapsedMilliseconds}ms.");
 
 
-var cab0 = blk.Cabs["CAB-0aa2768ea164a0d7db932b50052974af"];
 var root = (Transform)cab0.Objects.Values.Single(o => o is Transform { Father.PathId: 0 });
 PrintCab(cab0, root);
 //
@@ -50,21 +56,3 @@ T Point<T>(PPtr<T> pPtr, Cab cab) {
     // return (T)cab2.Objects[pPtr.PathId];
 }
 
-
-// void Write(long pathId, string ident="") {
-//     var t = (Transform)cab.ReadObject(cab.Objects[pathId]);
-//     var o = (GameObject)cab.ReadObject(cab.Objects[t.GameObject.PathId]);
-//     Console.WriteLine($"{ident}{o.Name}");
-//     foreach (var readObject in o.Components
-//                  .Select(c => cab.ReadObject(cab.Objects[c.PathId]))
-//                  .Where(readObject => readObject is not Transform)) {
-//         Console.WriteLine($"{ident}- {readObject}");
-//         // if (o.Name == "maoyou_body" && readObject is MonoBehaviour mb) {
-//         //     var script = cab.ReadObject(cab.Objects[mb.Script.PathId]);
-//         //     Console.WriteLine($"{ident}-- {mb.Name}::{script}");
-//         // }
-//     }
-//     foreach (var child in t.Children) {
-//         Write(child.PathId, ident+"  ");
-//     }
-// }
