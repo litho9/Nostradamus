b7158a4866da6b8c8afc5130713705cebd77c522
lioh <lithosmances@gmail.com>
2025-08-03 09:41:33 -0300

    refactor: ReadType

--- Nostradamus/Cab.cs
+++ Nostradamus/Cab.cs
@@ -54,7 +54,7 @@ public class Cab {
         if (!enableTypeTree) return type;
                 
         var numberOfNodes = reader.ReadInt32();
-        var stringBufferSize = reader.ReadInt32();
+        /*var stringBufferSize =*/ reader.ReadInt32();
         type.Nodes = Range(0, numberOfNodes).Select(_ => new TypeTreeNode {
             Version = reader.ReadUInt16(),
             Level = reader.ReadByte(),
@@ -66,145 +66,27 @@ public class Cab {
             MetaFlag = reader.ReadInt32(),
             RefTypeHash = reader.ReadUInt64() // >=19
         }).ToList();
-
-        using var memoryStream = new MemoryStream(reader.ReadBytes(stringBufferSize));
-        using (var stringBufferReader = new ObjectReader(memoryStream)) {
-            foreach (var node in type.Nodes) {
-                node.Type = ReadString(stringBufferReader, node.TypeStrOffset);
-                node.Name = ReadString(stringBufferReader, node.NameStrOffset);
-            }
+        foreach (var node in type.Nodes) {
+            node.Type = ReadString(reader, node.TypeStrOffset);
+            node.Name = ReadString(reader, node.NameStrOffset);
         }
-                
+
         if (isRefType) {
             type.KlassName = reader.ReadStringToNull();
             type.NameSpace = reader.ReadStringToNull();
             type.AsmName = reader.ReadStringToNull();
         } else {
-            type.TypeDependencies = Range(0, reader.ReadInt32()).Select(_ => reader.ReadInt32()).ToArray();
+            type.TypeDependencies = reader.ReadArray(_ => reader.ReadInt32());
         }
         return type;
         
         string ReadString(ObjectReader r, uint value) {
-            if ((value & 0x80000000) == 0) { // isOffset
-                r.BaseStream.Position = value;
-                return r.ReadStringToNull();
-            }
-            var offset = value & 0x7FFFFFFF;
-            return StringBuffer.TryGetValue(offset, out var str) ? str : offset.ToString();
+            if ((value & 0x80000000) != 0) // isOffset
+                return value.ToString();
+            r.BaseStream.Position = value;
+            return r.ReadStringToNull();
         }
     }
-
-    private static readonly Dictionary<uint, string> StringBuffer = new() {
-        {0, "AABB"},
-        {5, "AnimationClip"},
-        {19, "AnimationCurve"},
-        {34, "AnimationState"},
-        {49, "Array"},
-        {55, "Base"},
-        {60, "BitField"},
-        {69, "bitset"},
-        {76, "bool"},
-        {81, "char"},
-        {86, "ColorRGBA"},
-        {96, "Component"},
-        {106, "data"},
-        {111, "deque"},
-        {117, "double"},
-        {124, "dynamic_array"},
-        {138, "FastPropertyName"},
-        {155, "first"},
-        {161, "float"},
-        {167, "Font"},
-        {172, "GameObject"},
-        {183, "Generic Mono"},
-        {196, "GradientNEW"},
-        {208, "GUID"},
-        {213, "GUIStyle"},
-        {222, "int"},
-        {226, "list"},
-        {231, "long long"},
-        {241, "map"},
-        {245, "Matrix4x4f"},
-        {256, "MdFour"},
-        {263, "MonoBehaviour"},
-        {277, "MonoScript"},
-        {288, "m_ByteSize"},
-        {299, "m_Curve"},
-        {307, "m_EditorClassIdentifier"},
-        {331, "m_EditorHideFlags"},
-        {349, "m_Enabled"},
-        {359, "m_ExtensionPtr"},
-        {374, "m_GameObject"},
-        {387, "m_Index"},
-        {395, "m_IsArray"},
-        {405, "m_IsStatic"},
-        {416, "m_MetaFlag"},
-        {427, "m_Name"},
-        {434, "m_ObjectHideFlags"},
-        {452, "m_PrefabInternal"},
-        {469, "m_PrefabParentObject"},
-        {490, "m_Script"},
-        {499, "m_StaticEditorFlags"},
-        {519, "m_Type"},
-        {526, "m_Version"},
-        {536, "Object"},
-        {543, "pair"},
-        {548, "PPtr<Component>"},
-        {564, "PPtr<GameObject>"},
-        {581, "PPtr<Material>"},
-        {596, "PPtr<MonoBehaviour>"},
-        {616, "PPtr<MonoScript>"},
-        {633, "PPtr<Object>"},
-        {646, "PPtr<Prefab>"},
-        {659, "PPtr<Sprite>"},
-        {672, "PPtr<TextAsset>"},
-        {688, "PPtr<Texture>"},
-        {702, "PPtr<Texture2D>"},
-        {718, "PPtr<Transform>"},
-        {734, "Prefab"},
-        {741, "Quaternionf"},
-        {753, "Rectf"},
-        {759, "RectInt"},
-        {767, "RectOffset"},
-        {778, "second"},
-        {785, "set"},
-        {789, "short"},
-        {795, "size"},
-        {800, "SInt16"},
-        {807, "SInt32"},
-        {814, "SInt64"},
-        {821, "SInt8"},
-        {827, "staticvector"},
-        {840, "string"},
-        {847, "TextAsset"},
-        {857, "TextMesh"},
-        {866, "Texture"},
-        {874, "Texture2D"},
-        {884, "Transform"},
-        {894, "TypelessData"},
-        {907, "UInt16"},
-        {914, "UInt32"},
-        {921, "UInt64"},
-        {928, "UInt8"},
-        {934, "unsigned int"},
-        {947, "unsigned long long"},
-        {966, "unsigned short"},
-        {981, "vector"},
-        {988, "Vector2f"},
-        {997, "Vector3f"},
-        {1006, "Vector4f"},
-        {1015, "m_ScriptingClassIdentifier"},
-        {1042, "Gradient"},
-        {1051, "Type*"},
-        {1057, "int2_storage"},
-        {1070, "int3_storage"},
-        {1083, "BoundsInt"},
-        {1093, "m_CorrespondingSourceObject"},
-        {1121, "m_PrefabInstance"},
-        {1138, "m_PrefabAsset"},
-        {1152, "FileSize"},
-        {1161, "Hash128"}
-    };
 
     private static object ReadObject(ObjectInfo o, ObjectReader reader) {
         reader.BaseStream.Position = o.ByteStart;
@@ -261,19 +143,8 @@ public class ObjectReader(Stream input) : BinaryReader(input) {
     );
     public XForm ReadXForm() => new(ReadVector3(), ReadQuaternion(), ReadVector3());
 
-    public string ReadStringToNull(int maxLength = 32767) {
-        var bytes = new List<byte>();
-        var count = 0;
-        while (BaseStream.Length - BaseStream.Position > 0 && count < maxLength) {
-            var b = ReadByte();
-            if (b == 0) break;
-            bytes.Add(b);
-            count++;
-        }
-
-        return Encoding.UTF8.GetString(bytes.ToArray());
-    }
-    
+    private IEnumerable<byte> BytesToNull(byte b = 0) { while ((b = ReadByte()) != 0) yield return b; }
+    public string ReadStringToNull() => Encoding.UTF8.GetString(BytesToNull().ToArray());
     public string ReadAlignedString() => Align(() => Encoding.UTF8.GetString(ReadBytes(ReadInt32())));
 
     public T Align<T>(Func<T> fn, int alignment = 4) {
